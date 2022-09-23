cb2754b88a5c7a06003c94a2c5c9c3601f05be4b
Sumit <sumitsp198@gmail.com>
2022-09-23 17:42:20 +0530

    footer added

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -5,15 +5,18 @@ import {
 } from "react-router-dom";
 import Home from "./Components/Home"
 import Navbar from './Components/Navbar';
+import Footer from "./Components/Footer"
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      
       <Routes>
         <Route path="/" element={<Home  />} />
       </Routes>
+      <Footer/>
       
     </div>
   );
--- myntra/src/Components/Navbar.jsx
+++ myntra/src/Components/Navbar.jsx
@@ -7,45 +7,68 @@ const Navbar = () => {
   return (
     <div>
         <div className="navbar">
-            <NavLink to="/">
+            <NavLink to="/" >
                 <img className="mainlogo" alt="mainlogo" src="https://gumlet.assettype.com/afaqs%2F2021-01%2F15f5f827-8e29-4520-af8d-a0f353b8da17%2Fimages.png?auto=format%2Ccompress&w=1200"/>
             </NavLink>
-
-            <NavLink to="/men">MEN</NavLink>
-            <NavLink to="women">WOMEN</NavLink>
-            <NavLink to="/kids">KIDS</NavLink>
-            <NavLink to="/home&living">HOME & LIVING</NavLink>
-            <NavLink to="beauty">BEAUTY</NavLink>
-            <NavLink to="studio">STUDIO</NavLink>
+            <div className="menlink" >
+                <NavLink to="/men" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >MEN</NavLink>
+            </div>
+            <div className="womenlink">
+                <NavLink to="women" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >WOMEN</NavLink>
+            </div>
+            <div className="kidslink">
+                <NavLink to="/kids" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >KIDS</NavLink>
+            </div>
+            <div className="homelivinglink">
+                <NavLink to="/home&living" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >HOME & LIVING</NavLink>
+            </div>
+            <div className="beautylink">
+                <NavLink to="beauty" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >BEAUTY</NavLink>
+            </div>
+            <div className="studiolink">
+                <NavLink to="studio" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                >STUDIO</NavLink>
+            </div>
+            
+            
             <div className="searchbar">
                 <i id="sicon" class="fa-solid fa-magnifying-glass"></i>
                 <input type="text" placeholder="Search for products, brands and more"/>
             </div>
             <div className="nav">
-                <div className="profile">
-                    <NavLink to="/profile">
+                
+                    <NavLink to="/profile" style={{color:"#292d3f"}}>
                         <div>
-                            <i class="fa-regular fa-user"></i>
+                            <br/>
+                            <i class="fa-regular fa-user fa-lg"></i>
                             <p>Profile</p>
+                            
                         </div>                   
                     </NavLink>
-                </div>
-                <div className="wishlist">
-                    <NavLink to="/wishlist">
+                
+                
+                    <NavLink to="/wishlist" style={{color:"#292d3f"}}>
                         <div>
-                            <i class="fa-regular fa-heart"></i>
+                            <br/>
+                            <i class="fa-regular fa-heart fa-lg" ></i>
                             <p>Wishlist</p>
                         </div>                   
                     </NavLink>
-                </div>
-                <div className="bag">
-                    <NavLink to="/bag">
+                
+                
+                    <NavLink to="/bag" style={{color:"#292d3f"}}>
                         <div>
-                            <i class="fa-solid fa-bag-shopping"></i>
+                            <br/>
+                            <i class="fa-solid fa-bag-shopping fa-lg"></i>
                             <p>Bag</p>
                         </div>               
                     </NavLink>
-                </div>
+                
             </div>
             
         </div>
