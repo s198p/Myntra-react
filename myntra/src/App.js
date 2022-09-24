72df24082f9d6673da23346f4f2ac049a22d5863
Sumit <sumitsp198@gmail.com>
2022-09-24 10:54:42 +0530

    home

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={<Home  />} />
       </Routes>
       <Footer/>
+      {/* <Home/> */}
       
     </div>
   );
--- myntra/src/Components/Navbar.jsx
+++ myntra/src/Components/Navbar.jsx
@@ -11,27 +11,27 @@ const Navbar = () => {
                 <img className="mainlogo" alt="mainlogo" src="https://gumlet.assettype.com/afaqs%2F2021-01%2F15f5f827-8e29-4520-af8d-a0f353b8da17%2Fimages.png?auto=format%2Ccompress&w=1200"/>
             </NavLink>
             <div className="menlink" >
-                <NavLink to="/men" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="/men" style={{fontSize:"1rem",color:"#292d3f"}}
                 >MEN</NavLink>
             </div>
             <div className="womenlink">
-                <NavLink to="women" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="women" style={{fontSize:"1rem",color:"#292d3f"}}
                 >WOMEN</NavLink>
             </div>
             <div className="kidslink">
-                <NavLink to="/kids" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="/kids" style={{fontSize:"1rem",color:"#292d3f"}}
                 >KIDS</NavLink>
             </div>
             <div className="homelivinglink">
-                <NavLink to="/home&living" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="/home&living" style={{fontSize:"1rem",color:"#292d3f"}}
                 >HOME & LIVING</NavLink>
             </div>
             <div className="beautylink">
-                <NavLink to="beauty" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="beauty" style={{fontSize:"1rem",color:"#292d3f"}}
                 >BEAUTY</NavLink>
             </div>
             <div className="studiolink">
-                <NavLink to="studio" style={{fontSize:"1.2rem",color:"#292d3f"}}
+                <NavLink to="studio" style={{fontSize:"1rem",color:"#292d3f"}}
                 >STUDIO</NavLink>
             </div>
             
@@ -45,7 +45,7 @@ const Navbar = () => {
                     <NavLink to="/profile" style={{color:"#292d3f"}}>
                         <div>
                             <br/>
-                            <i class="fa-regular fa-user fa-lg"></i>
+                            <i class="fa-regular fa-user fa-md"></i>
                             <p>Profile</p>
                             
                         </div>                   
@@ -55,7 +55,7 @@ const Navbar = () => {
                     <NavLink to="/wishlist" style={{color:"#292d3f"}}>
                         <div>
                             <br/>
-                            <i class="fa-regular fa-heart fa-lg" ></i>
+                            <i class="fa-regular fa-heart fa-md" ></i>
                             <p>Wishlist</p>
                         </div>                   
                     </NavLink>
@@ -64,7 +64,7 @@ const Navbar = () => {
                     <NavLink to="/bag" style={{color:"#292d3f"}}>
                         <div>
                             <br/>
-                            <i class="fa-solid fa-bag-shopping fa-lg"></i>
+                            <i class="fa-solid fa-bag-shopping fa-md"></i>
                             <p>Bag</p>
                         </div>               
                     </NavLink>
